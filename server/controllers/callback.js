1da26314ea8853abf56769fd2c092108dd0719e1
hanamon <devparkhana@gmail.com>
2021-09-15 17:05:31 +0900

    [UPDATE] controllers

--- server/controllers/callback.js
+++ server/controllers/callback.js
@@ -1,3 +1,28 @@
+require('dotenv').config();
+const axios = require('axios');
+const clientID = process.env.GITHUB_CLIENT_ID;
+const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
 module.exports = (req, res) => {
-  res.send('ok')
-}
+  // req의 body로 authorizationCode가 들어온다.
+  // authorizationCode로 access token을 발급한다.
+  axios({
+    method: 'POST',
+    url: 'https://github.com/login/oauth/access_token',
+    headers: {
+      accept: 'application/json'
+    },
+    data: {
+      client_id: clientID,
+      client_secret: clientSecret,
+      code: req.body.authorizationCode
+    }
+  })
+  .then((res) => {
+    accessToken = res.data.access_token;
+    res.status(200).json({ accessToken: accessToken });
+  })
+  .catch((err) => {
+    res.sendStatus(404);
+  })
+};
--- server/controllers/images.js
+++ server/controllers/images.js
@@ -1,3 +1,10 @@
+const images = require('../resources/resources');
+
 module.exports = (req, res) => {
-  res.send('ok')
+  // 인증받은 회원(accessToken이 req.headers에 있는 경우)은
+  // 리소스에 접근할 권한을 부여받는다.
+  if( !req.headers.authorization ) {
+    return res.status(403).send({ message: 'no permission to access resources '});
+  }
+  res.status(200).send({ images });
 }
